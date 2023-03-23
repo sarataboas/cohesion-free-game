bfd8485e0b1938149ed54082eb6ac7d056302647
root <root@LAPTOP-5Q02QO6I.localdomain>
2023-03-23 13:36:17 +0000

    nhfg

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-board = [[0,0,0,0]
-         [0,0,0,0]
-         [0,0,0,0]
-         [0,0,0,0]
-         [0,0,0,0]]
+import pygame
 
+pygame.init()
+
+LARGURA_JANELA = 400
+ALTURA_JANELA = 400
+TAMANHO_CELULA = 100
+LARGURA_TABULEIRO = TAMANHO_CELULA * 4
+ALTURA_TABULEIRO = TAMANHO_CELULA * 4
+
+BRANCO = (255, 255, 255)
+
+janela = pygame.display.set_mode((LARGURA_JANELA, ALTURA_JANELA))
+pygame.display.set_caption("Cohesion Free Game")
+
+
+while True:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    janela.fill(BRANCO)
+
+    for linha in range(4):
+        for coluna in range(4):
+            cor = BRANCO
+
+            pygame.draw.rect(janela, cor, (coluna * TAMANHO_CELULA, linha * TAMANHO_CELULA, TAMANHO_CELULA, TAMANHO_CELULA))
+
+    pygame.display.update()
